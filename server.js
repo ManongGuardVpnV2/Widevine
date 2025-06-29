773d85d576789812ba4d2220c3504f838f5793d7
AnonymousHoax <108090689+ManongGuardVpnV2@users.noreply.github.com>
2025-06-29 16:19:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -28,3 +28,6 @@ app.post('/widevine-proxy', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Widevine proxy running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.send('✅ Widevine Proxy Server is running.');
+});
